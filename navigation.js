089d08170ee55b4cf1b5e0a8f143fa945b93dbe9
Amit-Singh-Gandhar <gandhar.amitsingh@gmail.com>
2017-11-24 01:28:40 +0530

    Design 24-11-2017
    New Tutorial Template

--- navigation.js
+++ navigation.js
@@ -3,16 +3,8 @@ function showMenu(){
     
     if(menu.className.indexOf("w3-show")==-1){
         menu.className+=" w3-show ";
-        homem.setAttribute("style","padding-top:6%");
-        homen.setAttribute("style","padding-top:6%");
-        homem.className = homem.className.replace("w3-padding-16","");
-        homen.className = homen.className.replace("w3-padding-16","");
     }
     else{
-        homem.setAttribute("style","padding-top:0");
-        homen.setAttribute("style","padding-rop:0");
         menu.className = menu.className.replace("w3-show","");
-        homem.className+=" w3-padding-16";
-        homen.className+=" w3-padding-16";
     }
 }
